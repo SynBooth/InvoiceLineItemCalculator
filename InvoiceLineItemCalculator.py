ec48d8ebc9458d92d32ff840c3dbbb1e24ea9be4
Olivia Booth <brokenstar15@gmail.com>
2023-11-12 17:15:23 -0500

    Began writing functions for price and quantity input

--- InvoiceLineItemCalculator.py
+++ InvoiceLineItemCalculator.py
@@ -5,3 +5,31 @@
 #Heading
 print("Welcome to the Invoice Line Item Calculator")
 
+#Function to asks user to input a price
+def inputPrice():
+    price = input("Enter a price: ")
+    print(f"Price: {price}")
+    return price
+    
+#function to ask user to input a quantity
+def inputQuantity():
+    quantity = input("Enter the quantity:  ")
+    print(f"Quantity: {quantity}")
+    return quantity
+
+
+        
+
+        
+
+
+
+
+
+
+#Testing Area
+inputPrice()
+inputQuantity()
+
+
+                            
